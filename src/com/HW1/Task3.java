f4fbefe98aadf6f5e46ab144c4c11698d9e3b085
yaroslav <yaroslavfgsf@gmail.com>
2020-12-06 12:03:30 +0200

    add Task3.java

--- src/com/HW1/Task3.java
+++ src/com/HW1/Task3.java
@@ -4,30 +4,49 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-
+/*
 public class Task3 {
+    private static String title;
+
+ */
+/*
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
 
-        int b =0;
-
-        String title;
+    boolean b;
 
 
 
-        do{
-            System.out.print("Введите число");
-            int a = reader.read();
-            System.out.print("Введите ");
-             title = reader.readLine();
-            b=b+a;
-        }while (title !="stop");
-        if(title=="stop"){
-            System.out.println(b);
-        }
 
 
 
     }
+
+
+ */
+
+ /*
+    public static void sum() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while(b=true) {
+            System.out.print("Введите число");
+            int a = reader.read();
+            int c = 0;
+            c += a;
+        }
+
+    }
+    public static void bool() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String title = reader.readLine();
+        if(title=="stop"){
+             b = false;
+        }
+    }
+
 }
+
+
+
+ */
+
---
+++ src/com/HW1/Tsk3.java
@@ -0,0 +1,30 @@
+package com.HW1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Tsk3 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int summa = 0;
+        int x = 0;
+
+
+        String a = "";
+
+        String b="stop";
+        while(!a.equals(b)){
+            x = reader.read();
+            a = reader.readLine();
+            summa +=x;
+        }
+        summa +=x;
+        System.out.println(summa);
+
+    }
+    }
+
+
